1cdef65714bfc7e5c1fe73da61f62bae44475789
Federico Irarrazaval <fedeirar@gmail.com>
2023-03-31 20:41:32 -0300

    mount vtex io

--- 1)service-course-template-master/node/clients/analytics.ts
+++ 1)service-course-template-master/node/clients/analytics.ts
@@ -1,3 +1,29 @@
-import { AppClient } from '@vtex/api'
+import { AppClient, InstanceOptions, IOContext } from '@vtex/api';
 
-export default class Analytics extends AppClient {}
+export default class Analytics extends AppClient {
+  constructor(context: IOContext, options?: InstanceOptions) {
+    super('vtex.mocked-analytics@0.0.4', context, options);
+  }
+
+  public async getLiveUsers(): Promise<LiveUsersProduct[]> {
+    return this.http.get('_v/live-products');
+    //   return [
+    //     {
+    //       slug: 'product1',
+    //       liveUsers: 10
+    //     },
+    //     {
+    //       slug: 'product2',
+    //       liveUsers: 20
+    //     }
+    //   ];
+    // }
+
+    //will get the necessary data for this application: an array of objects that have two fields: slug, a string that represents the product ID and liveUsers, a number that is the quantity of users visualizing this product - which are the fields in the interface.
+  }
+}
+
+interface LiveUsersProduct {
+  slug: string;
+  liveUsers: number;
+}
--- 1)service-course-template-master/node/index.ts
+++ 1)service-course-template-master/node/index.ts
@@ -4,23 +4,28 @@ import {
   ServiceContext,
   ParamsContext,
   RecorderState,
-  method,
-} from '@vtex/api'
-import { Clients } from './clients'
-import { analytics } from './handlers/analytics'
+  method
+} from '@vtex/api';
+import { Clients } from './clients';
+import { analytics } from './handlers/analytics';
+
+import { updateLiveUsers } from './event/liveUsersUpdate';
 
 // Create a LRU memory cache for the Status client.
 // The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
-const memoryCache = new LRUCache<string, any>({ max: 5000 })
-metrics.trackCache('status', memoryCache)
+const memoryCache = new LRUCache<string, any>({ max: 5000 });
+metrics.trackCache('status', memoryCache);
 
 declare global {
-  type Context = ServiceContext<Clients, State>
+  type Context = ServiceContext<Clients, State>;
 
   interface State extends RecorderState {
-    code: number
+    code: number;
   }
 }
+
+const THREE_SECONDS_MS = 3 * 1000;
+const CONCURRENCY = 10;
 
 export default new Service<Clients, State, ParamsContext>({
   clients: {
@@ -28,13 +33,25 @@ export default new Service<Clients, State, ParamsContext>({
     options: {
       default: {
         retries: 2,
-        timeout: 10000,
+        timeout: 10000
       },
-    },
+      events: {
+        exponentialTimeoutCoefficient: 2,
+        exponentialBackoffCoefficient: 2,
+        initialBackoffDelay: 50,
+        retries: 1,
+        timeout: THREE_SECONDS_MS,
+        concurrency: CONCURRENCY
+      }
+      //By adding this code to the Service, we are adding to the Client of this Service, the capability to handle events. At this point, we are not yet using the Client itself when handling the event.
+    }
   },
   routes: {
     analytics: method({
-      GET: [analytics],
-    }),
+      GET: [analytics]
+    })
   },
-})
+  events: {
+    liveUsersUpdate: updateLiveUsers //? This is the event handler for the event liveUsersUpdate that we created in the file event/liveUsersUpdate.ts and exported as updateLiveUsers. This is the function that will be called when the event liveUsersUpdate is triggered.
+  }
+});
